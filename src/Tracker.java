6d0b116b0d6c226277dbc12b6039e7d0fc8a00f4
Levi Malott <lmnn3@mst.edu>
2014-09-26 11:30:10 -0500

    First code written!

---
+++ src/Tracker.java
@@ -0,0 +1,11 @@
+import java.lang.Long;
+import java.lang.String;
+
+public class Tracker
+{
+    public String type;
+    public String filename;
+    public Long   filesize;
+    public String desc;
+    public byte[] md5_val;
+}
