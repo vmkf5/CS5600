4258d6eb7beaffff2c53ff7866f509a8237f2a04
Levi Malott <lmnn3@mst.edu>
2014-10-20 13:06:16 -0500

    Fixed MD5 calculation of initial files, added test main function.

--- src/client/Peer.java
+++ src/client/Peer.java
@@ -23,7 +23,7 @@ public class Peer
     private Integer segment_size;
     private HashMap<String,SharedFileDetails> shared_files;
     private ArrayList<FileTracker> current_downloads;
-    private InetAddress my_ip;
+    private String my_ip;
     private static String REQ_LIST = "<REQ LIST>\n";
     private static Integer MAX_SEGMENT_SIZE = 1024;
     private RespList tracker_list;
@@ -64,7 +64,7 @@ public class Peer
         in           = null;
         tracker_list = null;
         try {
-            my_ip = InetAddress.getLocalHost();
+            my_ip = InetAddress.getLocalHost().getHostAddress();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
@@ -80,12 +80,12 @@ public class Peer
         InputStream input = null;
         try
         {
-            input = new FileInputStream("config.properties");
+            input = new FileInputStream(filename);
             prop.load(input);
 
-            this.server_port = Integer.parseInt(prop.getProperty("server_port", "8081"));
+            this.server_port = Integer.parseInt(prop.getProperty("server_port", "8080"));
             this.server_ip   = InetAddress.getByName(prop.getProperty("server_ip", "localhost"));
-            this.my_port   = Integer.parseInt(prop.getProperty("peer_port", "8080"));
+            this.my_port   = Integer.parseInt(prop.getProperty("peer_port", "8081"));
             this.refresh_rate= Integer.parseInt(prop.getProperty("refresh_rate", "60"));
             this.share_dir   = prop.getProperty("share_dir", "shares");
             this.segment_size= Integer.parseInt(prop.getProperty("segment_size"));
@@ -117,7 +117,7 @@ public class Peer
         String[] files = f.list();
         for (String filename : files)
         {
-            CallbackFileInit d = new CallbackFileInit(filename, " ", this);
+            CallbackFileInit d = new CallbackFileInit(share_dir,filename, " ", this);
             Thread t = new Thread(d);
             t.start();
         }
@@ -128,7 +128,7 @@ public class Peer
      * @param filename
      * @param info
      */
-    private synchronized void initFile(String filename, SharedFileDetails info)
+    public synchronized void initFile(String filename, SharedFileDetails info)
     {
         shared_files.put(filename, info);
         sendCreateTracker(info);
@@ -145,6 +145,7 @@ public class Peer
             socket = new Socket(server_ip, server_port);
             out    = new PrintWriter(socket.getOutputStream(), true);
             in     = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println("Connected to server:" + server_ip.toString());
         }
         catch (UnknownHostException e)
         {
@@ -190,11 +191,12 @@ public class Peer
         String resp = null;
         try {
              msg = new createFileTrackerMessage(info.filename, info.filesize, info.description,
-                    info.md5, this.my_ip.toString(), this.my_port);
+                    info.md5, this.my_ip, this.my_port);
         } catch (CreateTrackerException e) {
             e.printStackTrace();
         }
         out.println(msg.toString());
+        System.out.println("Sent message: " + msg.toString());
         try {
             resp = in.readLine();
         } catch (IOException e) {
@@ -206,6 +208,8 @@ public class Peer
             System.out.println("Unknown response from server from: " + msg.toString());
             System.exit(1);
         }
+
+        System.out.println(resp);
 
         switch(response)
         {
@@ -221,6 +225,12 @@ public class Peer
             default: break;
         }
         return "";
+    }
+
+    public void getFileTracker(Integer i)
+    {
+        String filename = tracker_list.getFilenameAt(i);
+        this.getFileTracker(filename);
     }
 
     /**
@@ -248,6 +258,7 @@ public class Peer
         //To compute the MD5 hash, the header and tail of the message are ignored
         //then put all the data into one string and compute the hash
         String content =  message.subList(1,message.size()-1).toString().replaceAll("\\[|\\]", "").replaceAll(", ", "\n");
+        content = content.substring(1,content.length()-1);
         content = getMd5(content);
         if( content.equals(md5_recvd))
         {
@@ -378,5 +389,14 @@ public class Peer
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    public static void main(String[] args)
+    {
+        Peer peer = new Peer("/home/levi/IdeaProjects/CS5600/src/data/config.properties");
+        peer.startFileSenderManager();
+
+
     }
 }
